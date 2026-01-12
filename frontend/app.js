74bef6ba184d5b63379ea6bffc5fe10d7bf4a401
caviles913 <christopher.avi@edu.312school.com>
2026-01-11 23:02:15 -0600

    Added backend url

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-var baseUrl = 'http://a2747d9e3dda6485992f7f3e0ac69b7d-1936667441.us-east-1.elb.amazonaws.com:80'; // Address for the backend/API server
+var baseUrl = 'http://a632fea70745b4240a971590693c600b-673399434.us-east-1.elb.amazonaws.com:80'; // Address for the backend/API server
 
 // Add product
 $("#product-form").submit(function (e) {
